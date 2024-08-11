65e7b155b90d62a15f9eca3c3fe1b82faf84d4f4
Khushi Khari <144012000+khushikhari@users.noreply.github.com>
2024-08-11 20:43:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,7 @@ import streamlit as st
 import pickle
 import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.ensemble import RandomForestClassifier
+
 
 
 # Load the machine learning model
